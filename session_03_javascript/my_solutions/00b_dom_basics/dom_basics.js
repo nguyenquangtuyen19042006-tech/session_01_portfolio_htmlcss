2b1a895d780ddf421dd46960db9128d77829db7e
nguyenquangtuyen19042006-tech <nguyenquangtuyen19042006@gmail.com>
2026-06-01 09:22:41 -0700

    [UI] Apply styles and class list manipulation

--- session_03_javascript/my_solutions/00b_dom_basics/dom_basics.js
+++ session_03_javascript/my_solutions/00b_dom_basics/dom_basics.js
@@ -5,3 +5,7 @@ const descriptions = document.querySelectorAll('.description');
 // [FEATURE] Change content
 title.textContent = "Chào DOM!";
 descriptions[0].innerHTML = "<b>Đã thay đổi nội dung</b>";
+
+// [UI] Apply styles and classes
+title.style.color = "blue";
+descriptions[1].classList.add('highlight');
