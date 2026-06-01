85dc3120510949c2a4b59aa5165c623f1da8254b
nguyenquangtuyen19042006-tech <nguyenquangtuyen19042006@gmail.com>
2026-06-01 09:27:08 -0700

    [FEATURE] Implement input event listener

--- session_03_javascript/my_solutions/00c_events/events.js
+++ session_03_javascript/my_solutions/00c_events/events.js
@@ -3,3 +3,9 @@ const btn = document.getElementById('colorBtn');
 btn.addEventListener('click', () => {
     document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
 });
+
+// [FEATURE] Input event
+const input = document.getElementById('userInput');
+input.addEventListener('input', (e) => {
+    console.log("Đang nhập:", e.target.value);
+});
