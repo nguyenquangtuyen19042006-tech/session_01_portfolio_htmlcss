d36d060a6216cb00df3a78d668762ab81a3186af
nguyenquangtuyen19042006-tech <nguyenquangtuyen19042006@gmail.com>
2026-06-01 09:34:33 -0700

    [DOM] Render student list to table

--- session_03_javascript/my_solutions/00e_student_app/app.js
+++ session_03_javascript/my_solutions/00e_student_app/app.js
@@ -4,3 +4,16 @@ const sinhVien = [
     { maSV: "SV002", hoTen: "Trần Thị B", diemTB: 7.2 }
 ];
 console.log("Dữ liệu đã khởi tạo:", sinhVien);
+
+// [DOM] Render danh sách sinh viên
+function renderTable() {
+    const tbody = document.querySelector('tbody');
+    tbody.innerHTML = sinhVien.map(sv => `
+        <tr>
+            <td>${sv.maSV}</td>
+            <td>${sv.hoTen}</td>
+            <td>${sv.diemTB}</td>
+        </tr>
+    `).join('');
+}
+renderTable();
