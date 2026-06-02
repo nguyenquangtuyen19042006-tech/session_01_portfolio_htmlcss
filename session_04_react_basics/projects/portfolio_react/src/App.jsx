cd9fd067a1aeeb442a75526d802fa0cdb4414b8a
nguyenquangtuyen19042006-tech <nguyenquangtuyen19042006@gmail.com>
2026-06-02 02:42:41 -0700

    [LAYOUT] Assemble App.jsx layout

--- session_04_react_basics/projects/portfolio_react/src/App.jsx
+++ session_04_react_basics/projects/portfolio_react/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// src/App.jsx
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -7,20 +7,24 @@ import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
+// Import dữ liệu tĩnh từ data file
+import { skills, projects } from './data/portfolio';
+
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Skills />
-        <Portfolio />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header />
+            <main>
+                <Hero />
+                <About />
+                {/* Truyền trực tiếp mảng dữ liệu tĩnh qua props */}
+                <Skills skills={skills} />
+                <Portfolio projects={projects} />
+                <Contact />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
